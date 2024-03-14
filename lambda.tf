6a232e33cb796278c054682b617ff8a224b66849
Rahilz313 <126873703+Rahilz313@users.noreply.github.com>
2024-03-14 15:54:16 +0530

    Create lambda.tf

---
+++ lambda.tf
@@ -0,0 +1,11 @@
+resource "aws_lambda_function" "my_lambda_function" {
+  function_name = "Jenkins-terraform-automation"
+  role          = "arn:aws:iam::655625281801:role/LambdaS3Role"
+  handler       = "lambda_function.lambda_handler"  # Specify the handler function in your Docker image
+
+  runtime = "provided.al2"  # Use "provided.al2" for custom runtime like Docker
+
+  image_uri = "655625281801.dkr.ecr.us-east-1.amazonaws.com/terraform-repo:latest"
+
+  package_type = "Image"
+}
