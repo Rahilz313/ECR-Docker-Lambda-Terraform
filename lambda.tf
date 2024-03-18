a763d9e8190e2eebf8d13ad790413d9312141754
Rahilz313 <126873703+Rahilz313@users.noreply.github.com>
2024-03-18 12:27:27 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "my_lambda_function" {
   function_name = "Jenkins-terraform-automation"
   role          = "arn:aws:iam::655625281801:role/LambdaS3Role"
-  handler       = "lambda_function.lambda_handler"  # Specify the handler function in your Docker image
+ 
 
   runtime = "provided.al2"  # Use "provided.al2" for custom runtime like Docker
 
